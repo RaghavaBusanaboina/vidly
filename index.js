1fa6a86a44ef6e59d0333426a7532ef5ae6a4cf9
Raghava <busanaboinaraghava@gmail.com>
2023-01-09 18:48:36 +0530

    initial vidly commit

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // export vidly_jwtPrivateKey=mySecurekey
-const winston = require("winston");
-const express = require("express");
+const winston = require('winston');
+const express = require('express');
 const app = express();
 
-require("./startup/logging")();
-require("./startup/routes")(app);
-require("./startup/db")();
-require("./startup/config")();
-require("./startup/validation")();
-require("./startup/prod")(app);
+require('./startup/logging')();
+require('./startup/routes')(app);
+require('./startup/db')();
+require('./startup/config')();
+require('./startup/validation')();
+require('./startup/prod')(app);
 const port = process.env.PORT || 4000;
-app.listen(port, () => winston.info(`Listening on port ${port}...`));
+app.listen(port, () => console.log(`Listening on port ${port}...`));
--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,13 @@
-const winston = require("winston");
-const mongoose = require("mongoose");
-const config = require("config");
+const winston = require('winston');
+const mongoose = require('mongoose');
+const config = require('config');
 module.exports = function () {
-  const db = config.get("db");
+  const db = config.get('db');
   mongoose
-    .connect(db, { useNewUrlParser: true })
-    .then(() => winston.info(`Connected to ${db}...`));
+    .connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      autoIndex: true,
+    })
+    .then(() => console.log(`Connected to $db...`));
 };
